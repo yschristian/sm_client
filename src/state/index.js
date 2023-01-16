bf2b7a22eb6d4268fcd252e08183b6308a3e3c28
yschristian <yubahwesc@gmail.com>
2023-01-16 20:58:19 +0200

    state redux

--- src/state/index.js
+++ src/state/index.js
@@ -4,7 +4,7 @@ const initialState = {
     mode: "light",
     user: null,
     token: null,
-    post: []
+    posts: []
 
 }
 
@@ -19,14 +19,29 @@ export const authSlice = createSlice({
             state.user = action.payload.user;
             state.token = action.payload.token
         },
-        setLogout: (state) =>{
+        setLogout: (state) => {
             state.user = null;
             state.token = null
         },
         setFriends: (state, action) => {
-            if(state.user){
-                
+            if (state.user) {
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error("user friends non-existent:(")
             }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts
+        },
+        setPost: (state, action) => {
+            const updatedPosts = state.posts.map((post) => {
+                if (post._id === action.payload.post._id) return action.payload.post
+                return post;
+            })
+            state.posts = updatedPosts;
         }
     }
 })
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost, } = authSlice.actions
+export default authSlice.reducer
