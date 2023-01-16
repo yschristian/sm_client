28d7b2cbcf639bc9b4da8355a69e41e5256e4087
yschristian <yubahwesc@gmail.com>
2023-01-16 20:59:03 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
-import HomePage from "pages/homePage"
-// import LoginPage from "pages/loginPage"
-// import profilePage from "pages/profilePage"
+import HomePage from "./pages/homePage"
+import LoginPage from "./pages/loginPage"
+import profilePage from "./pages/profilePage"
 
 const App = () => {
 
@@ -10,8 +10,8 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/login" element={<LoginPage />} /> */}
-          {/* <Route path="/profile/:userId" element={< profilePage />} /> */}
+          <Route path="/login" element={<LoginPage />} />
+           <Route path="/profile/:userId" element={< profilePage />} />
         </Routes>
       </BrowserRouter>
     </div>
