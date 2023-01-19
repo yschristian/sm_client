04027b351d100f994a25a9bdde5e70dcb7fcc520
yschristian <yubahwesc@gmail.com>
2023-01-19 11:57:29 +0200

    navbar

--- src/pages/navbar/index.jsx
+++ src/pages/navbar/index.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
           fontWeight="bold"
           fontSize="clamp(1rem, 2rem , 2.25rem)"
           color="primary"
-          onClick={() => navigate("/")}
+          onClick={() => navigate("/home")}
           sx={{
             "&:hover": {
               color: primarylLight,
