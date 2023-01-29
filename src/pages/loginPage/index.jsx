ebd5b085d6037d5eb42a68e12b5504568d7d2ea6
yschristian <yubahwesc@gmail.com>
2023-01-29 16:24:25 +0200

    w travel

--- src/pages/loginPage/index.jsx
+++ src/pages/loginPage/index.jsx
@@ -1,20 +1,22 @@
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
-
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
-  const theme = useTheme()
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   return (
     <Box>
-      <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%">
-        <Typography
-          fontWeight="bold"
-          fontSize="32px"
-          color="primary"
-        >
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
           WeTravel
         </Typography>
       </Box>
+
       <Box
         width={isNonMobileScreens ? "50%" : "93%"}
         p="2rem"
@@ -23,11 +25,12 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to WeTravel
+          Welcome to WeTravel, the Social Media for Traveling Person!
         </Typography>
+        <Form />
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
